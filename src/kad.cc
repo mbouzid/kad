be0f9cb8fcdedca9ecd2b9c83bcf7252c060d5ff
Jérôme Audoux <jerome.audoux@gmail.com>
2017-04-07 17:17:15 +0200

    Encode kmer key as 64bit integers

--- src/kad.cc
+++ src/kad.cc
@@ -1,19 +1,96 @@
 #include <zlib.h>
 #include <iostream>
 #include <rocksdb/db.h>
+#include <rocksdb/slice.h>
 #include <cassert>
 #include <stdlib.h>
 #include <jansson.h>
+#include <math.h> // floor()
+#include <cinttypes>
 
 #include "kseq.h"
 #include "kstring.h"
 
 KSEQ_INIT(gzFile, gzread)
 
-#define KAD_VERSION "0.0.2"
+#define KAD_VERSION "0.0.3"
 #define LEVELDB_PATH "/tmp/testdb"
 
+#define KMER_LENGTH 32
+
+enum DNA_MAP {C, A, T, G};  // A=1, C=0, T=2, G=3
+static const char NUCLEOTIDES[4] = { 'C', 'A', 'T', 'G' };
+
 using namespace std;
+
+uint64_t str_to_int(char* str)
+{
+  uint64_t strint = 0;
+  for (size_t i = 0; i < KMER_LENGTH; i++) {
+    uint8_t curr = 0;
+    switch (str[i]) {
+      case 'A': { curr = DNA_MAP::A; break; }
+      case 'T': { curr = DNA_MAP::T; break; }
+      case 'C': { curr = DNA_MAP::C; break; }
+      case 'G': { curr = DNA_MAP::G; break; }
+    }
+    strint = strint << 2;
+    strint = strint | curr;
+  }
+  return strint;
+}
+
+string int_to_str(uint64_t kmer)
+{
+  uint8_t base;
+  string str;
+  for (int i=KMER_LENGTH; i>0; i--) {
+    base = (kmer >> (i*2-2)) & 3ULL;
+    char chr;
+		switch(base) {
+			case DNA_MAP::A: { chr = 'A'; break; }
+			case DNA_MAP::T: { chr = 'T'; break; }
+			case DNA_MAP::C: { chr = 'C'; break; }
+			case DNA_MAP::G: { chr = 'G'; break; }
+		}
+    str.push_back(chr);
+  }
+  return str;
+}
+
+
+int kad_test(int argc, char **argv) {
+  char kmer[33] = "AGAGGAGGGACGGGCTGAAAAAGTACTCATTG";
+
+  uint64_t kmer_int = str_to_int(kmer);
+
+  string kmer_str = int_to_str(kmer_int);
+  cout << kmer << "\t" << int_to_str(kmer_int) << endl;
+  
+
+  char *buf = (char*)malloc(sizeof(uint64_t));
+  memcpy(buf, (char*)&kmer_int,sizeof(uint64_t));
+  
+  uint64_t kmer_int_found;
+  memcpy(&kmer_int_found, buf, sizeof(uint64_t));
+
+  cout << int_to_str(kmer_int) << endl;
+
+  //char *buf = malloc(sizeof(kmer_int));
+  //memcpy(buf, &kmer_int, sizeof(kmer_int));
+  //Slice(buf, sizeof(kmer_int));
+
+  //string key;
+  //PutFixed64(&key,kmer_int);
+
+  //char packed_kmer[KMER_PACKED_LENGTH];
+  //char unpacked_kmer[KMER_LENGTH + 1];
+  //unpacked_kmer[KMER_LENGTH] = '\0';
+  //pack_kmer(kmer,packed_kmer);
+  //unpack_kmer(packed_kmer,unpacked_kmer);
+  //cout << kmer << "\t" << packed_kmer << "\t" << unpacked_kmer << endl;
+  return 0;
+}
 
 int kad_dump(int argc, char **argv)
 {
@@ -25,7 +102,9 @@ int kad_dump(int argc, char **argv)
 
   rocksdb::Iterator* it = db->NewIterator(rocksdb::ReadOptions());
   for (it->SeekToFirst(); it->Valid(); it->Next()) {
-    std::cout << it->key().ToString() << ": "  << it->value().ToString() << std::endl;
+    uint64_t kmer_int_found;
+    memcpy(&kmer_int_found, it->key().data(), sizeof(uint64_t));
+    std::cout << int_to_str(kmer_int_found) << ": "  << it->value().ToString() << std::endl;
   }
   return 0;
 }
@@ -46,7 +125,11 @@ int kad_query(int argc, char **argv) {
 
   char *kmer = argv[1];
 
-  rocksdb::Status s = db->Get(rocksdb::ReadOptions(), kmer, &value);
+  uint64_t kmer_int = str_to_int(kmer);
+
+  rocksdb::Slice key((char*)&kmer_int, sizeof(uint64_t));
+
+  rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &value);
   if (s.ok()) {
     std::cout << kmer << "\t" << value << std::endl;
   }
@@ -76,6 +159,7 @@ int kad_index(int argc, char **argv)
 	kstream_t *ks;
 	kstring_t *str,*kmer;
   int dret, count;
+  uint64_t kmer_int;
 
   kmer  = (kstring_t*)calloc(1, sizeof(kstring_t));
   str   = (kstring_t*)calloc(1, sizeof(kstring_t));
@@ -89,7 +173,10 @@ int kad_index(int argc, char **argv)
     if(dret != '\n') {
       if(ks_getuntil(ks, 0, str, &dret) > 0 && isdigit(str->s[0])) {
         count = atoi(str->s);
-        //std::cerr << kmer->s << "\t" << count << std::endl;
+        
+        kmer_int = str_to_int(kmer->s);
+
+        rocksdb::Slice key((char*)&kmer_int, sizeof(uint64_t));
 
         json_t *obj; 
         json_t *counts;
@@ -97,7 +184,7 @@ int kad_index(int argc, char **argv)
         json_t *count_value = json_integer(count);
         json_object_set_new(count_entry,sample_name,count_value);
 
-        rocksdb::Status s = db->Get(rocksdb::ReadOptions(), kmer->s, &value);
+        rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &value);
 
         if(s.ok()) {
           obj = json_loads(value.c_str(),JSON_DECODE_ANY,NULL);
@@ -112,7 +199,7 @@ int kad_index(int argc, char **argv)
 
         char *json = json_dumps(obj,JSON_COMPACT);
         
-        s = db->Put(rocksdb::WriteOptions(), kmer->s, json);
+        s = db->Put(rocksdb::WriteOptions(), key, json);
 
         json_decref(counts);
         json_decref(obj);
@@ -147,6 +234,7 @@ int main(int argc, char *argv[])
 	if (strcmp(argv[1], "index") == 0) kad_index(argc-1, argv+1);
   else if (strcmp(argv[1], "dump") == 0) kad_dump(argc-1, argv+1);
   else if (strcmp(argv[1], "query") == 0) kad_query(argc-1, argv+1);
+  else if (strcmp(argv[1], "test") == 0) kad_test(argc-1, argv+1);
 	else {
 		fprintf(stderr, "[main] unrecognized command '%s'. Abort!\n", argv[1]);
 		return 1;
